8be6d76a7852ac091a821a71960994a30563d105
Quách Đức Thành <97529743+jeffthanh@users.noreply.github.com>
2023-05-01 18:10:48 +0700

    backend

---
+++ controllers/api.js
@@ -0,0 +1,76 @@
+const Post = require("../models/posts");
+const fs = require('fs');
+
+
+module.exports = class API {
+    static async fetchAllPost(req, res) {
+        try {
+            const post = await Post.find();
+            res.status(200).json(post);
+        } catch (err) {
+            res.status(404).json({ message: err.message });
+        }
+    }
+    static async fetchPostID(req, res) {
+        const id = req.params.id;
+        try {
+            const post = await Post.findById(id);
+            res.status(200).json(post);
+        } catch (err) {
+            res.status(404).json({ message: err.message });
+        }
+    }
+    static async createPost(req, res) {
+        const post = req.body;
+        if (req.file) {
+            const imagename = req.file.filename;
+            post.image = imagename;
+        }
+        try {
+            await Post.create(post);
+            res.status(201).json({ message: "Post created !!!" });
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    }
+
+    static async updetaPost(req, res) {
+        const id = req.params.id;
+        let new_image = "";
+        if (req.file) {
+            new_image = req.file.filename;
+            try {
+                fs.unlinkSync("./uploads/" + req.body.old_image);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            new_image = req.body.old_image;
+        }
+        const newPost = req.body;
+        newPost.image = new_image;
+        try {
+            await Post.findByIdAndUpdate(id, newPost);
+            res.status(200).json({ message: "Post updated !!!" });
+        } catch (err) {
+            res.status(404).json({ message: err.message });
+        }
+    }
+    static async deletePost(req, res) {
+        const id = req.params.id;
+        try {
+            const result = await Post.findByIdAndDelete(id);
+            if (result && result.image != '') {
+                try {
+                    fs.unlinkSync('./uploads/' + result.image);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+            res.status(200).json({ message: "Post Deleted !!!" });
+        } catch (err) {
+            res.status(404).json({ message: err.message });
+        }
+    }
+
+}
